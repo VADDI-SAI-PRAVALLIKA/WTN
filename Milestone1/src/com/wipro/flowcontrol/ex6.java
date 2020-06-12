c823203369f6ec1b46174b1d8295c1d3e70c09f4
VADDI SAI PRAVALLIKA <vaddi.saipravallika@gmail.com>
2020-06-12 09:58:22 +0530

    percentage of interest

---
+++ Milestone1/src/com/wipro/flowcontrol/ex6.java
@@ -0,0 +1,26 @@
+package com.wipro.flowcontrol;
+
+public class ex6 {
+	public static void main(String[] args)
+	{
+		String g=args[0];
+		int age=Integer.parseInt(args[1]);
+		if(g.equals("Female") && age>1 && age<58)
+		{
+			System.out.println("Percentage of interest is 8.2%.");
+		}
+		else if(g.equals("Female")&& age>59 && age<100)
+		{
+			System.out.println("Percentage of interest is 9.2%.");
+		}
+		else if(g.equals("Male")&& age>1 && age<58)
+		{
+			System.out.println("Percentage of interest is 8.4%.");
+		}
+		else if(g.equals("Male")&& age>59 && age<100)
+		{
+			System.out.println("Percentage of interest is 10.5%.");
+		}
+	}
+
+}
