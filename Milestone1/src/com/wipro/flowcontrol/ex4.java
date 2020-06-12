f0fb2b3faae552ca658b0903e6a59b2a7656ddb4
VADDI SAI PRAVALLIKA <vaddi.saipravallika@gmail.com>
2020-06-12 09:56:58 +0530

    Alphabetical

---
+++ Milestone1/src/com/wipro/flowcontrol/ex4.java
@@ -0,0 +1,22 @@
+package com.wipro.flowcontrol;
+
+public class ex4 {
+	public static void main(String[] args)
+	{
+		if(args.length>0)
+		{
+			char a='s';
+			char b='e';
+			if(a<b)
+			{
+				System.out.println(a+" , "+b);
+			}
+			else
+			{
+				System.out.println(b+" , "+a);
+			}
+			
+		}
+	}
+
+}
