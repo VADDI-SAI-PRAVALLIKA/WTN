ac70f2e201f52871b05a8c406ade0adc862f6f47
VADDI SAI PRAVALLIKA <vaddi.saipravallika@gmail.com>
2020-06-12 10:01:15 +0530

    print b/w 23 and 57

---
+++ Milestone1/src/com/wipro/flowcontrol/ex11.java
@@ -0,0 +1,15 @@
+package com.wipro.flowcontrol;
+
+public class ex11 {
+	public static void main(String[] args)
+	{
+		for(int i=24;i<57;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
