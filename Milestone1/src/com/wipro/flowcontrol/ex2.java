fb33720ee0f2d8d8917724c576ebb5578d893eb6
VADDI SAI PRAVALLIKA <vaddi.saipravallika@gmail.com>
2020-06-12 09:55:24 +0530

    odd or even

---
+++ Milestone1/src/com/wipro/flowcontrol/ex2.java
@@ -0,0 +1,17 @@
+package com.wipro.flowcontrol;
+
+public class ex2 {
+	public static void main(String[] args)
+	{
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+	}
+
+}
