2952a504b70e9444885e52c761b2657607fd69cf
VADDI SAI PRAVALLIKA <vaddi.saipravallika@gmail.com>
2020-06-18 10:28:29 +0530

    sum of all digits

---
+++ Milestone1/src/com/wipro/flowcontrol/ex14.java
@@ -0,0 +1,15 @@
+package com.wipro.flowcontrol;
+
+public class ex14 {
+	public static void main(String[] args)
+	{
+		int n=Integer.parseInt(args[0]);
+		int i;
+		for(i=0;n>0;n=n/10)
+		{
+			i=i+(n%10);
+		}
+		System.out.println(i);
+	}
+
+}
